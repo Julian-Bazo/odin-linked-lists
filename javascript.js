60bd13e3fad79498da593d1de9d5676608f509d1
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-10-07 12:01:40 -0400

    Add node search
    Adds the at(index) function which allows for search capabilities.

--- javascript.js
+++ javascript.js
@@ -56,7 +56,29 @@ class LinkedList {
         console.log(this.tail);
     }
     
+    at(index) {
+        let i = 0;
+        let currentNode = this.head;
+        if (index > LinkedList.nodeNum - 1 || index < 0) {
+            console.error("Index does not exist");
+        }
+        console.log("Index found:");
+        if (index === 0) {
+            console.log(currentNode);
+            return currentNode;
+        }
+        while (i < index) {
+            currentNode = currentNode.nextNode;
+            i++;
+        }
+        console.log(currentNode);
+        return currentNode;
+    }
 
+    // pop() {
+    //     let newTail = at(LinkedList.nodeNum - 1);
+    //     this.tail = newTail;
+    // }
 
 }
 
@@ -74,6 +96,8 @@ newList.append("second");
 newList.append("third");
 newList.prepend("zero");
 newList.append("End");
+newList.at(4);
+// newList.pop();
 
 
 newList.getHead();
