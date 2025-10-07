0e00b2e5516378591ff3fe054cfe619ba6fcbf38
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-10-07 10:57:55 -0400

    Add first class functions
    Adds the first three class functions to the LinkedList class.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,51 @@
-console.log("hi");
+class LinkedList {
+
+    static nodeNum = 0;
+
+    constructor(name) {
+        this.name = name;
+        this.head = null;
+    }
+
+    append(nodeName) {
+        LinkedList.nodeNum++;
+        const newNode = new Node(nodeName);
+        if (this.head === null) {
+            this.head = newNode;
+        }
+    }
+
+    prepend(nodeName) {
+        LinkedList.nodeNum++;
+        const newNode = new Node(nodeName);
+        this.head = newNode;
+    }
+
+    size() {
+        console.log(`Node count: ${LinkedList.nodeNum}`);
+    }
+
+    getHead() {
+        console.log(this.head)
+    }
+    
+
+
+}
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+const newList = new LinkedList("Default");
+console.log(newList);
+newList.append("test");
+newList.getHead();
+console.log(newList);
+newList.prepend("secondary");
+newList.size();
+newList.getHead();
+
